7037f76175782f94e2af3ab2546b25f89b8abb53
seun awoga <seunsmooth@yahoo.com>
2020-11-28 11:12:26 +0000

    latest update 11:12

--- code_deploy.sh
+++ code_deploy.sh
@@ -7,5 +7,5 @@ echo $PACKER_LOG
 echo $PACKER_LOG_PATH
 AMI_IDa=`packer build -machine-readable ../redis_blue_green/packer/packer.json | tee build.log ` 
 AMI_ID=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
-echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf 
-terraform apply -auto-approve 
+echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > ../redis_blue_green/terraform/amivar.tf 
+#terraform apply -auto-approve 
